a1a2e6bf337a2dbf2281be4f5a0bcdd40789f4af
negevking <74301525+negevking@users.noreply.github.com>
2023-05-04 11:22:24 +0200

    added two clean files with condition indicator and degradation model

--- Main_Files/code/Data/calculate_SK.m
+++
@@ -1,9 +0,0 @@
-function [SK, F] = calculate_SK(table1, variable1, wc)
-%wc 128 for vibration or 10 for acceleration/tach
-
-data = read(table1);
-v = data{:,variable1}{1,1};
-fs = 97656;
-[SK, F] = pkurtosis(v, fs, wc);
-
-end
